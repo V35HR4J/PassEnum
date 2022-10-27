d9bbfef4049c0cfe322d8a9643dea8aaecdbb818
4vian5 <chhetria191@mytru.ca>
2022-10-27 10:21:04 -0700

    fixed copy button

--- myscripts.js
+++ myscripts.js
@@ -1,7 +1,10 @@
-pass='';
+pass=[];
 pass_list = ["1","321","@123","123","1234","@1234","12345","123456","1234567","1234568"]
 var arrayLength = pass_list.length;
 function printName() {
+    pass = []
+    hello=document.querySelector('#wordlist');
+    hello.innerHTML = ""
     firstName = document .getElementById("first").value;
     upper=firstName[0].toUpperCase()+firstName.slice(1);
     lower=firstName[0].toLowerCase()+firstName.slice(1);
@@ -9,28 +12,29 @@ function printName() {
     if(firstName.includes("a")){
         replaced=firstName.replace(/a/gi, "@");
         for (var i = 0; i < arrayLength; i++) {
-            pass+=upper+pass_list[i]+'<br>';
-            pass+=lower+pass_list[i]+'<br>';
-            pass+=replaced+pass_list[i]+'<br>';
+            pass.push(upper+pass_list[i]);
+            pass.push(lower+pass_list[i]);
+            pass.push(replaced+pass_list[i]);
         }
     }
     else{
         for (var i = 0; i < arrayLength; i++) {
-            pass+=upper+pass_list[i]+'<br>';
-            pass+=lower+pass_list[i]+'<br>';
+            pass.push(upper+pass_list[i]);
+            pass.push(lower+pass_list[i]);
     }
 }
-hello=document.querySelector('#wordlist');
-hello.innerHTML = pass;
+
+var textarea = document.getElementById('wordlist');
+textarea.value = '';
+
+pass.forEach(item => {
+    textarea.value += item + '\n';
+})
+
 }
 
 function copyFunc(){
-    var range = document.createRange();
-    range.selectNode(document.getElementById("wordlist"));
-    window.getSelection().removeAllRanges(); // clear current selection
-    window.getSelection().addRange(range); // to select text
-    document.execCommand("copy");
-    window.getSelection().removeAllRanges();// to deselect
-    alert('Copied to Clipboard')
+    document.querySelector("textarea").select();
+    document.execCommand('copy');
 }
     
