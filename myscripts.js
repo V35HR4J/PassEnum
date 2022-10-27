30baaa6b89883885b5d5e04bb3c2cb9ef293ebda
yaman1337 <yaman.sarabariya@gmail.com>
2022-10-27 20:25:47 +0545

    remove innerHtml to avoid xss

--- myscripts.js
+++ myscripts.js
@@ -1,7 +1,8 @@
-pass='';
+pass=[];
 pass_list = ["1","321","@123","123","1234","@1234","12345","123456","1234567","1234568"]
 var arrayLength = pass_list.length;
 function printName() {
+    hello=document.querySelector('#wordlist');
     firstName = document .getElementById("first").value;
     upper=firstName[0].toUpperCase()+firstName.slice(1);
     lower=firstName[0].toLowerCase()+firstName.slice(1);
@@ -9,19 +10,24 @@ function printName() {
     if(firstName.includes("a")){
         replaced=firstName.replace(/a/gi, "@");
         for (var i = 0; i < arrayLength; i++) {
-            pass+=upper+pass_list[i]+'<br>';
-            pass+=lower+pass_list[i]+'<br>';
-            pass+=replaced+pass_list[i]+'<br>';
+            pass.push(upper+pass_list[i]);
+            pass.push(lower+pass_list[i]);
+            pass.push(replaced+pass_list[i]);
         }
     }
     else{
         for (var i = 0; i < arrayLength; i++) {
-            pass+=upper+pass_list[i]+'<br>';
-            pass+=lower+pass_list[i]+'<br>';
+            pass.push(upper+pass_list[i]);
+            pass.push(lower+pass_list[i]);
     }
 }
-hello=document.querySelector('#wordlist');
-hello.innerHTML = pass;
+
+pass.forEach(item => {
+    const p = document.createElement("p");
+    p.textContent = item
+    hello.append(p)
+})
+
 }
 
 // TODO:
