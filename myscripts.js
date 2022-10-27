8b5d3db98f608ce54958fe790640623a3e4c53bb
Sankalpa-Acharya <sankalpaacharya6609@gmail.com>
2022-10-27 23:47:02 +0530

    generates list in word wave form

--- myscripts.js
+++ myscripts.js
@@ -1,44 +1,63 @@
-pass=[];
-pass_list = ["1","321","@123","123","1234","@1234","12345","123456","1234567","1234568"]
+pass = [];
+pass_list = ["1", "321", "@123", "123", "1234", "@1234", "12345", "123456", "1234567", "1234568"]
 var arrayLength = pass_list.length;
-function printName() {
-    pass = []
-    hello=document.querySelector('#wordlist');
-    hello.innerHTML = ""
-    firstName = document .getElementById("first").value;
-    upper=firstName[0].toUpperCase()+firstName.slice(1);
-    lower=firstName[0].toLowerCase()+firstName.slice(1);
-    replace=firstName.replace(/a/g, "@");
-    if(firstName.includes("a")){
-        replaced=firstName.replace(/a/gi, "@");
+
+//Wave the word.
+function word_updown() {
+    pass = [];
+    word = document.getElementById("first").value;
+    word = word.toLowerCase();
+    word = word.split('');
+    for (i in word) {
+        word[i] = word[i].toUpperCase();
+        letter = word.join('');
+        wordlist_maker(letter);
+        word[i] = word[i].toLowerCase();
+    }
+
+    printName();
+}
+
+function wordlist_maker(word) {
+    firstName = word || document.getElementById("first").value;
+    first_letter_uppercase = firstName[0].toUpperCase() + firstName.slice(1);
+    first_letter_lowercase = firstName[0].toLowerCase() + firstName.slice(1);
+    if (firstName.includes("a")) {
+        replaced = firstName.replace(/a/gi, "@");
         for (var i = 0; i < arrayLength; i++) {
-            pass.push(upper+pass_list[i]);
-            pass.push(lower+pass_list[i]);
-            pass.push(replaced+pass_list[i]);
+            pass.push(first_letter_uppercase + pass_list[i]);
+            pass.push(first_letter_lowercase + pass_list[i]);
+            pass.push(replaced + pass_list[i]);
         }
     }
-    else{
+    else {
         for (var i = 0; i < arrayLength; i++) {
-            pass.push(upper+pass_list[i]);
-            pass.push(lower+pass_list[i]);
+            pass.push(first_letter_uppercase + pass_list[i]);
+            pass.push(first_letter_lowercase + pass_list[i]);
+        }
     }
-}
-
-pass.forEach(item => {
-    const p = document.createElement("p");
-    p.textContent = item
-    hello.append(p)
-})
 
 }
+// prints all password in pass array to html element
+function printName() {
+    hello = document.querySelector('#wordlist');
+    hello.innerHTML = ""
+    pass.forEach(item => {
+        const p = document.createElement("p");
+        p.textContent = item
+        hello.append(p)
+    })
+}
 
-function copyFunc(){
+
+// copy to clipboard
+function copyFunc() {
     var range = document.createRange();
     range.selectNode(document.getElementById("wordlist"));
     window.getSelection().removeAllRanges(); // clear current selection
     window.getSelection().addRange(range); // to select text
     document.execCommand("copy");
     window.getSelection().removeAllRanges();// to deselect
-    alert('Copied to Clipboard')
+    alert('Copied to Clipboard');
 }
-    
+
